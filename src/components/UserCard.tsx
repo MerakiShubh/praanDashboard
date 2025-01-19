f49dc697de243ac70db23b7d154c37af0487553c
MerakiShubh <shubhamt.champ@gmail.com>
2025-01-19 21:17:10 +0530

    diplay cards

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function UserCard({ type }: { type: string }) {
   return (
-    <div className="rounded-2xl odd:bg-shubhPurple even:bg-shubhYellow p-4 flex-1 min-w-[130px] ">
+    <div className="rounded-2xl odd:bg-praanPurple even:bg-praanYellow p-4 flex-1 min-w-[130px] ">
       <div className="flex justify-between items-center">
         <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
           2024/25
