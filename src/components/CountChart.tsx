6604259c59479979eb0ed1d79823c6d2cbae4913
MerakiShubh <shubhamt.champ@gmail.com>
2025-01-19 23:47:20 +0530

    add calendar component

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -62,15 +62,15 @@ const style = {
 
 function CountChart() {
   return (
-    <div className="">
+    <div className="bg-white rounded-xl w-full h-full p-4">
       <div className="flex justify-between items-center">
         {/* Title  */}
-        <h1>Students</h1>
-        <Image src="moreDark.png" alt="" width={20} height={20} />
+        <h1 className="text-lg font-semibold">Students</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
       </div>
-      <div>
-        {/* Chart  */}
-        <ResponsiveContainer width="100%" height="100%">
+      {/* Chart  */}
+      <div className="w-full h-[75%]">
+        <ResponsiveContainer>
           <RadialBarChart
             cx="50%"
             cy="50%"
@@ -80,22 +80,27 @@ function CountChart() {
             data={data}
           >
             <RadialBar
-              minAngle={15}
               label={{ position: "insideStart", fill: "#fff" }}
               background
-              clockWise
               dataKey="uv"
             />
-            <Legend
-              iconSize={10}
-              layout="vertical"
-              verticalAlign="middle"
-              wrapperStyle={style}
-            />
+            <Legend iconSize={10} layout="vertical" verticalAlign="middle" />
           </RadialBarChart>
         </ResponsiveContainer>
       </div>
-      <div>{/* Bottom  */}</div>
+      {/* BOTTOM  */}
+      <div className="flex justify-center gap-16">
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-praanSky rounded-full" />
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Boys(55%)</h2>
+        </div>
+        <div className="flex flex-col gap-1">
+          <div className="w-5 h-5 bg-praanYellow rounded-full" />
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Girls(45%)</h2>
+        </div>
+      </div>
     </div>
   );
 }
